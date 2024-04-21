f78f8c1eeb00e7550a2adebceccb0287a029c6d8
afshanzunaib <163379124+afshanzunaib@users.noreply.github.com>
2024-04-21 03:22:04 -0700

    Add files via upload

---
+++ atm.ts
@@ -0,0 +1,67 @@
+import inquirer from "inquirer";
+
+let mybalance = 10000;
+let mypin = 1234;
+
+ let pinAnswer = await inquirer.prompt([{
+    
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+}]
+
+ );
+
+  if (pinAnswer.pin === mypin)
+    console.log("check pin code!!!");
+
+    let oprationAns = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                message:"please select option",
+                type:"list",
+                choices:["withdraw", "cheak balance"],
+            }
+        ]
+    );
+
+
+    console.log("oprationAns");
+  
+    if (oprationAns.operation === "withdraw"){
+        let amountAns = await inquirer.prompt([
+
+        
+        {
+name: "amount",
+message: "enter your amount",
+type: "number",
+
+
+
+      
+}
+
+
+
+    ]
+);
+
+if(amountAns.amount <= mybalance && amountAns.amount >= 500){
+mybalance -= amountAns.amount;
+console.log("your remaning balance is:" + mybalance);
+    }
+    else if (oprationAns.operation === "check balance"){
+        console.log("your balance is:" + mybalance);
+    }
+ else{
+    console.log("invaild amount");
+}
+    }  
+    else if(oprationAns.operation === "cheak balance"){
+        console.log("your balance is:" + mybalance);
+    }
+    else {
+        console.log("invalid pin");
+    }
